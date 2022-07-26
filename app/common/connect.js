04bb911e4972eb99b6513781d4b0b3c014b6bb3e
Vu Ngan Yen <yen.vu@amitgroup.vn>
2022-07-26 10:56:36 +0700

    test

--- app/common/connect.js
+++ app/common/connect.js
@@ -1,5 +1,4 @@
 var mysql = require('mysql')
-
 var connection = mysql.createConnection({
     host : 'localhost',
     user:'admin',
@@ -15,5 +14,5 @@ connection.connect(function (err){
     else
     console.log("connect success");
  })
- 
+
 module.exports = connection
